797e516993a84343bf882e7f0dd5325457edf4d8
harshit <upadhyayharshit05@gmail.com>
2023-08-14 12:12:41 +0530

    path change

--- src/DB/data.js
+++ src/DB/data.js
@@ -1,33 +1,33 @@
 const Data=[
     {
-        img:'src/assets/breakfast-img.png',
+        img:'/assets/breakfast-img.png',
         title:'Breakfast',
         button:'SEE MORE',       
     },
-    {
-        img:'src/assets/coffee-img.png',
-        title:'Coffee',
-        button:'SEE MORE',       
-    },
-    {
-        img:'src/assets/delivery-img.png',
-        title:'Delivery',
-        button:'SEE MORE',       
-    },
-    {
-        img:'src/assets/dinner-img.png',
-        title:'Dinner',
-        button:'SEE MORE',       
-    },
-    {
-        img:'src/assets/diningtable-img.png',
-        title:'Diningtable',
-        button:'SEE MORE',       
-    },
-    {
-        img:'src/assets/alcohol-img.png',
-        title:'Drinks',
-        button:'SEE MORE',       
-    },
+    // {
+    //     img:'public/assets/coffee-img.png',
+    //     title:'Coffee',
+    //     button:'SEE MORE',       
+    // },
+    // {
+    //     img:'public/assets/delivery-img.png',
+    //     title:'Delivery',
+    //     button:'SEE MORE',       
+    // },
+    // {
+    //     img:'public/assets/dinner-img.png',
+    //     title:'Dinner',
+    //     button:'SEE MORE',       
+    // },
+    // {
+    //     img:'public/assets/diningtable-img.png',
+    //     title:'Diningtable',
+    //     button:'SEE MORE',       
+    // },
+    // {
+    //     img:'public/assets/alcohol-img.png',
+    //     title:'Drinks',
+    //     button:'SEE MORE',       
+    // },
 ]
 export default Data
